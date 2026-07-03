18c5b45603c85de7fa397a800a609131dbf8abc3
Bruna Lopes <brunaafl@usp.br>
2026-07-03 13:27:08 +0200

    Add condition on yield statement

--- neuralset-repo/neuralset/extractors/image.py
+++ neuralset-repo/neuralset/extractors/image.py
@@ -183,10 +183,10 @@ class HuggingFaceImage(extractor_base.BaseStatic, hf.HuggingFaceMixin):
                     # notes: - aggregating with a batch would be slightly more efficient
                     # but code would be messier
                     # - aggregating in cuda avoids transferring too much data to cpu
-                    latent = self._aggregate_tokens(latent).cpu().numpy()
+                    latent = self._aggregate_tokens(latent)
                     if aggregate_layers:
                         latent = self._aggregate_layers(latent)
-                    yield latent
+                    yield latent.cpu().numpy() if isinstance(latent, torch.Tensor) else latent
 
     @infra.apply(
         item_uid=_huggingface_image_event_uid,
